670bce76d946150d7bfbed254375064fa73d2aab
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-01-31 14:57:47 +0100

    add fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+
+  fetch("http://localhost:5500/")
+    .then((res) => {
+      res.json().then((data) => {
+        setData(data.message);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
   return (
     <>
@@ -21,15 +32,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>{data}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(PORT, (err) => {
