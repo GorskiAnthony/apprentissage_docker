2377b2a34007b2922a162ebfc08e1a9f385732f8
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-01-31 14:53:00 +0100

    add client

--- server/src/index.js
+++ server/src/index.js
@@ -13,6 +13,6 @@ app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`Server is running on port $s`, PORT);
+    console.log(`Server is running on port %s`, PORT);
   }
 });
