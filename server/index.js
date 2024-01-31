37e6ef87848c5b551329b682cfe976d9870a7b30
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-01-31 14:26:53 +0100

    add dotenv

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is running on port $s`, PORT);
+  }
+});
